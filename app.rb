40758afa8d5420bba552749f3d5e8c6291192a49
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-24 00:28:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,5 +8,9 @@ class App < Sinatra::Base
    get '/' do 
      erb :index 
    end 
-   
+    post '/checkout' do
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+  end
 end
